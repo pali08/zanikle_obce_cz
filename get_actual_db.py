85092e134f68b45fbd8595770ca114b3aa38ca74
pali08 <p.mikulaj@gmail.com>
2021-11-03 23:58:22 +0100

    WIP: Work on saving data to pickle - creating of DB is done. TODO: test it and save to pickle.

--- get_actual_db.py
+++ get_actual_db.py
@@ -18,6 +18,7 @@ EASTEST_POINT = 18.85889
 NORTHEST_POINT = 51.05556
 SOUTHEST_POINT = 48.55251
 DECIMAL_PLACES_COORDS = 100000
+SAMPLING = 100
 
 
 # print(table)
@@ -47,39 +48,69 @@ def get_number_of_pages():
     return highest_page
 
 
-def create_empty_database_with_coords_as_key(sampling=100):
+def create_empty_database_with_coords_as_key(sampling=SAMPLING):
     eastest = int(((EASTEST_POINT * DECIMAL_PLACES_COORDS) // sampling) + 1)
     westest = int((WESTEST_POINT * DECIMAL_PLACES_COORDS) // sampling)
     northest = int(((NORTHEST_POINT * DECIMAL_PLACES_COORDS) // sampling) + 1)
     southest = int((SOUTHEST_POINT * DECIMAL_PLACES_COORDS) // sampling)
     print(eastest - westest)
     print(northest - southest)
-    return [[[] for longitude in range(0, northest - southest)] for latitude in range(0, eastest - westest)]
+    return [[[] for latitude in range(0, northest - southest)] for longitude in range(0, eastest - westest)]
 
 
-# print(create_empty_database_with_coords_as_key(10))
-create_empty_database_with_coords_as_key(100)
-
-
-def append_to_database_with_coords_as_key(list_of_places, web_address, sampling=100):
+def append_to_database_with_coords_as_key(list_of_places, web_address, sampling=SAMPLING):
     data_of_place = get_data_of_place(web_address)
-    longitude_index = int(((data_of_place['N'] - SOUTHEST_POINT) * DECIMAL_PLACES_COORDS) / sampling)
-    latitude_index = int(((data_of_place['E'] - WESTEST_POINT) * DECIMAL_PLACES_COORDS) / sampling)
-    list_of_places[latitude_index][longitude_index].append(data_of_place)
-    for i in range(0, len(list_of_places)):
-        for j in range(0, len(list_of_places[i])):
-            try:
-                list_of_places[i][j][0]
-                print('index is' + str(i) + ', ' + str(j))
-            except IndexError:
-                pass
+    indices = get_indices_from_coordinates(data_of_place, sampling=sampling)
+    list_of_places[indices[0]][indices[1]].append(data_of_place)
+    # for i in range(0, len(list_of_places)):
+    #     for j in range(0, len(list_of_places[i])):
+    #         try:
+    #             list_of_places[i][j][0]
+    #             print('index is' + str(i) + ', ' + str(j))
+    #         except IndexError:
+    #             pass
     return list_of_places
 
 
-empty_list_of_lost_places = create_empty_database_with_coords_as_key(sampling=100)
+def get_indices_from_coordinates(place, sampling=SAMPLING):
+    try:
+        latitude = place['N']
+    except IndexError:
+        latitude = place['S']
+    try:
+        longitude = place['E']
+    except IndexError:
+        longitude = place['W']
+    latitude_index = int(((latitude - SOUTHEST_POINT) * DECIMAL_PLACES_COORDS) / sampling)
+    longitude_index = int(((longitude - WESTEST_POINT) * DECIMAL_PLACES_COORDS) / sampling)
+    return longitude_index, latitude_index
 
-append_to_database_with_coords_as_key(empty_list_of_lost_places,
-                                      'http://www.zanikleobce.cz/index.php?obec=26519', sampling=100)
+
+# empty_list_of_lost_places = create_empty_database_with_coords_as_key(sampling=100)
+#
+# append_to_database_with_coords_as_key(empty_list_of_lost_places,
+#                                       'http://www.zanikleobce.cz/index.php?obec=26519', sampling=100)
+
+
+def get_database_of_lost_places_as_list():
+    num_of_pages = get_number_of_pages()
+    places_as_list = create_empty_database_with_coords_as_key(sampling=SAMPLING)
+    for i in range(1, num_of_pages + 1):
+        url_lost_places = 'http://www.zanikleobce.cz/index.php?menu=93&sort=1&l=&str=' + str(i)
+        response = requests.get(url_lost_places)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table_lost_places = soup.find('table')
+        for tr in table_lost_places.findAll("tr"):
+            tds = tr.findAll("td")
+            try:
+                link = tds[0].find('a')['href']
+                if 'obec=' in link:
+                    append_to_database_with_coords_as_key(places_as_list, url_main + link, sampling=SAMPLING)
+            except AttributeError as attr_error:
+                print('attribute error - this should not be problem. Stacktrace follows' + str(attr_error))
+            except Exception as any_exception:
+                print('Exception occurred:' + str(any_exception) + 'stack trace follows')
+                print(traceback.format_exc())
 
 
 def get_database_of_lost_places(path, is_test=False):
