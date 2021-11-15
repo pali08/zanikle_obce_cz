9d1a191ee12174c4afba81213145dc65a1d9c13a
pali08 <p.mikulaj@gmail.com>
2021-11-15 02:20:24 +0100

    Cosmetic change

--- tests/test_database_handling_queries.py
+++ tests/test_database_handling_queries.py
@@ -5,7 +5,8 @@ from database_handling_queries import get_distance
 
 class MyTestCase(unittest.TestCase):
     def test_get_distance(self):
-        self.assertAlmostEqual(get_distance(52.2296756, 21.0122287, 52.406374, 16.9251681), 279.35290160430094)  # add assertion here
+        self.assertAlmostEqual(get_distance(52.2296756, 21.0122287, 52.406374, 16.9251681),
+                               279.35290160430094)  # add assertion here
 
 
 if __name__ == '__main__':
