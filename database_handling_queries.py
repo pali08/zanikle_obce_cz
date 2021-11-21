4813c47beeb2dbe0935fa4e2b1cefd8ca384bf8d
pali08 <p.mikulaj@gmail.com>
2021-11-21 03:03:21 +0100

    Get places by municipality added

--- database_handling_queries.py
+++ database_handling_queries.py
@@ -3,6 +3,8 @@ import sqlite3
 
 import geopy.distance
 
+DB_FILE = 'database.db'
+
 
 def get_db_cursor(db_file):
     con = sqlite3.connect(db_file)
@@ -23,6 +25,17 @@ def get_places_in_radius(specified_point, radius, db_connection):
     cursor = db_connection.cursor()
     cursor.execute('SELECT * FROM database_lost_places WHERE getdistance({}, {}, north, east) < {}'.format(
         str(specified_point_latitude), str(specified_point_longitude), str(radius)))
+    rows = cursor.fetchall()
+    db_connection.close()
+    return rows
+
+
+# print(get_places_in_radius([49.19669, 16.60292], 20, sqlite3.connect(DB_FILE)))
+
+
+def get_places_by_municipality(municipality, db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT * FROM database_lost_places WHERE municipality={}'.format(municipality))
     rows = cursor.fetchall()
     db_connection.close()
     return rows
