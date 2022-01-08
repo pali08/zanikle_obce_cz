a00dd009fe31ff77cb535c9ee9efd8d428c6ca2a
pali08 <p.mikulaj@gmail.com>
2022-01-08 23:16:16 +0100

    Name of town is not case sensitive.

--- database_handling_queries.py
+++ database_handling_queries.py
@@ -34,7 +34,7 @@ def get_places_in_radius(specified_point, radius, db_connection):
 
 def get_places_by_municipality(municipality, db_connection):
     cursor = db_connection.cursor()
-    cursor.execute('SELECT * FROM database_lost_places WHERE municipality=\'{}\''.format(municipality))
+    cursor.execute('SELECT * FROM database_lost_places WHERE lower(municipality)=lower(\'{}\')'.format(municipality))
     rows = cursor.fetchall()
     db_connection.close()
     return rows
